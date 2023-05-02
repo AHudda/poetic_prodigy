9421b10cc8c582ac37b30816b51bd0bae22f7e82
Mason Zhang <mzhang1709@gmail.com>
2023-05-02 15:50:54 -0400

    loss works

--- model.py
+++ model.py
@@ -38,13 +38,44 @@ def get_disc_model(units, optimizer):
     return model
 
 
-cc_func = tf.keras.losses.CategoricalCrossentropy()
+cc_func = tf.keras.backend.binary_crossentropy
+acc_func = tf.keras.metrics.binary_accuracy
+# g_l = tf.Variable(tf.random.truncated_normal([29, 1]))
+# d_l = tf.Variable(tf.random.truncated_normal([29, 1]))
+# d_r = tf.Variable(tf.random.truncated_normal([29, 1]))
 
 def g_loss(d_fake:tf.Tensor) -> tf.Tensor:
     return tf.reduce_mean(cc_func(tf.ones_like(d_fake), d_fake))
 
 def d_loss(d_fake:tf.Tensor, d_real:tf.Tensor)  -> tf.Tensor:
-    d_fake_loss = cc_func(tf.zeros_like(d_fake), d_fake)
-    d_real_loss = cc_func(tf.ones_like(d_real), d_real)
-    return tf.reduce_mean(d_fake_loss + d_real_loss)
+    d_fake_loss = tf.reduce_mean(cc_func(tf.zeros_like(d_fake), d_fake))
+    d_real_loss = tf.reduce_mean(cc_func(tf.ones_like(d_real), d_real))
+    return d_fake_loss + d_real_loss
+
+def d_acc_fake(d_fake:tf.Tensor, d_real:tf.Tensor)  -> tf.Tensor:
+    ideal=tf.zeros_like(d_fake) #using fake data, how many times was it fake and discriminator could tell it was fake #prediction that discriminator says the fake thing is fake
+    return acc_func(ideal, d_fake) 
+
+def d_acc_real(d_fake:tf.Tensor, d_real:tf.Tensor)  -> tf.Tensor:
+    ideal=tf.ones_like(d_real) #acc real we use 1s like and real data
+    return acc_func(ideal, d_real)
+
+def g_acc(d_fake:tf.Tensor, d_real:tf.Tensor) -> tf.Tensor:
+    ideal=tf.ones_like(d_fake) #prob it's able to fool discriminator. zeros-like - how good discriminator is true
+    return acc_func(ideal, d_fake) #how many times is it fooling discriminator
+
+# print("g: ", g_loss(g_l))
+# print("d: ", d_loss(d_l, d_r))
+
+# print(d_loss(tf.constant([0., 0., 0.]), tf.constant([1., 1., 1.])).numpy())
+# print(d_loss(tf.constant([0., 0., 0.]), tf.constant([0., 0., 0.])).numpy())
+# print(d_loss(tf.constant([1., 1., 1.]), tf.constant([1., 1., 1.])).numpy())
+# print(d_loss(tf.constant([1., 1., 1.]), tf.constant([0., 0., 0.])).numpy())
+# print()
+# print(g_loss(tf.constant([1., 1., 1.]), None).numpy())
+# print(g_loss(tf.constant([0., 0., 0.]), None).numpy())
+# print()
+# print(d_acc_fake(tf.constant([.1, .9, .9]), None).numpy())
+# print(d_acc_real(None, tf.constant([.1, .9, .9])).numpy())
+# print(g_acc(tf.constant([.1, .9, .9]), None).numpy())
 
--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from preprocess import get_data
-from model import get_gen_model, gumbel_softmax, get_disc_model, g_loss, d_loss
+from model import get_gen_model, gumbel_softmax, get_disc_model, g_loss, d_loss, d_acc_fake, d_acc_real, g_acc
 
 EPOCHS = 1
 BATCH_SIZE = 30
@@ -10,7 +10,10 @@ UNITS = 200
 EMBED = 128
 
 def train(train_data, vocab_dict):
-    total_loss = 0
+    total_g_loss = 0
+    total_d_loss = 0
+    avg_gen_acc = 0
+    avg_dis_acc = 0
     g_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
     d_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
     
@@ -27,33 +30,44 @@ def train(train_data, vocab_dict):
             #batch = batch.reshape((30, 1)) 
             batch = batch.reshape((batch.shape[0], 1)) 
             x_fake = gumbel_softmax(generator(batch)) #(batch size, window size, vocab size)
-            print('generator value for x_fake: ', x_fake)
+            # print('generator value for x_fake: ', x_fake)
             d_fake = discriminator(x_fake)
-            print("generator value for d_fake: ", d_fake)
+            # print("generator value for d_fake: ", d_fake)
             loss = g_loss(d_fake)
+            # print("g: ", loss)
+            total_g_loss += loss
+            # acc = g_acc(x_fake, None)
+            # print("g: ", acc)
+            # avg_gen_acc += acc
         grads = tape.gradient(loss, generator.trainable_variables)
         g_optimizer.apply_gradients(zip(grads, generator.trainable_variables))
-        print('finished generator loss section')
+        # print('finished generator loss section')
 
         #calculate and apply discriminator gradients, get the real loss too
         with tf.GradientTape() as tape:
             x_real = generator(batch)
             x_fake = gumbel_softmax(generator(batch)) #(batch size, window size, vocab size)
-            print('discriminator value for x_fake: ', x_fake)
+            # print('discriminator value for x_fake: ', x_fake)
             d_fake = discriminator(x_fake)
-            print("discriminator value for d_fake: ", d_fake)
+            # print("discriminator value for d_fake: ", d_fake)
             d_real = discriminator(x_real)
             loss = d_loss(d_fake, d_real)
-            total_loss += loss
+            # print("d: ", loss)
+            total_d_loss += loss
+            # f_acc = d_acc_fake(d_fake, None)
+            # r_acc = d_acc_real(None, d_real)
+            # avg_dis_acc += (f_acc *x_fake.shape[0] + r_acc*x_real.shape[0])/(x_fake.shape[0] + x_real.shape[0])
         grads = tape.gradient(loss, discriminator.trainable_variables)
         d_optimizer.apply_gradients(zip(grads, discriminator.trainable_variables))
-        print('finished discriminator loss section')
+        # print('finished discriminator loss section')
+        #remove all print statements when you start training model for faster
+    return total_g_loss, total_d_loss, avg_gen_acc/num_batches, avg_dis_acc/num_batches
 
 
 train_data, vocab_dict = get_data("data/Poetry.txt")
 for epoch_id in range(EPOCHS):
-    total_loss = train(train_data, vocab_dict)
-    print("Training Epoch: ", epoch_id, " and Loss: ", total_loss)
+    total_g, total_d, acc_g, acc_d = train(train_data, vocab_dict)
+    print("Training Epoch: ", epoch_id, " and Generator Loss: ", total_g, " and Discriminator Loss: ", total_d) #, "and Generator Acc: ", acc_g, " and Discriminator Acc: ", acc_d)
     #print("Training Epoch: ", epoch_id, " and Loss: ", total_loss/len(train_data))
 
 
